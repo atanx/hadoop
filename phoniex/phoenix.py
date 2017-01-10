3ee6ade89119416581e00cb1731b6c9081abcc9a
07jiangbin <07jiangbin@163.com>
2017-01-10 11:28:02 +0800

    添加python脚本可执行sql文件

---
+++ phoniex/phoenix.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import phoenixdb
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+database_url = 'http://localhost:8765/'
+conn = phoenixdb.connect(database_url, autocommit=True)
+cursor = conn.cursor()
+
+
+def get_cmd_type(cmd):
+	if len(cmd.strip()) == 0 or cmd.startswith('!'):
+		return 'invalid_cmd'
+	kw = cmd.split()[0].upper()
+	if kw in ('SELECT','SHOW'):
+		return "data_cmd"
+	else:
+		return "no_data_cmd"
+
+
+
+
+def execute_sql(cmd):
+	cmd_type= get_cmd_type(cmd)
+	if cmd_type == "data_cmd":
+		cursor.execute(cmd)
+		data = cursor.fetchall()
+		for row in data:
+			for idx, item in enumerate(row):
+				sys.stdout.write(str(item))
+				if idx < len(row)-1:
+					sys.stdout.write('\t')
+			sys.stdout.write('\n')
+	elif cmd_type == "no_data_cmd":
+		cursor.execute(cmd)
+	else:
+		pass
+
+
+def execute_sqlfile(sqlfile):
+	content = open(sqlfile).read()
+	cmds = [ i.strip() for i in content.split(';')]
+	for cmd in cmds:
+		execute_sql(cmd)
+
+
+if __name__ == '__main__':
+	
+	if len(sys.argv)==2:
+		execute_sqlfile(sys.argv[1])
+	else:
+		raise SyntaxError(u"语法错误，未指定SQL文件")
+	
